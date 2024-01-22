5a004ac15a1ee893008ff93164efb4187660cad5
curiouscosmos <onecuriouscosmos@gmail.com>
2024-01-22 16:14:21 -0500

    Initial

---
+++ TSL SDK/TSL_SDKApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSL_SDKApp.swift
+//  TSL SDK
+//
+//  Created by Daman Mehta on 2024-01-22.
+//
+
+import SwiftUI
+
+@main
+struct TSL_SDKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
