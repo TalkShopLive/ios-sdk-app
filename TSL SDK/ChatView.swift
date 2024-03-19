68998ffef99e6603a64dce906524c2f696025607
Mayuri <mayuri.contractor@talkshop.live>
2024-03-19 16:52:50 -0400

    IOS-48 : Chat - Count Messages

--- TSL SDK/ChatView.swift
+++ TSL SDK/ChatView.swift
@@ -12,7 +12,8 @@ struct ChatView: View {
     var showID = "vzzg6tNu0qOv"
     var eventID = "8WtAFFgRO1K0"
     @State private var chat: Talkshoplive.Chat? = nil
-    
+    @State private var result: String = ""
+
     
     var body: some View {
         VStack {
@@ -61,10 +62,23 @@ struct ChatView: View {
             .background(Color.black)
             .cornerRadius(10)
             
+            // Message Count
+            Button("Count Messages") {
+                countMessages()
+            }
+            .frame(width: 240)
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.black)
+            .cornerRadius(10)
+            
             // Render Token or User Id
             if (chat != nil) {
                 Text("Token successfully created!")
                     .padding()
+                
+                // Show success
+                Text(result).padding()
             }
         }
         .colorScheme(.light)
@@ -110,6 +124,21 @@ struct ChatView: View {
         let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzZGtfMmVhMjFkZTE5Y2M4YmM1ZTg2NDBjN2IyMjdmZWYyZjMiLCJleHAiOjE3OTkyNjc3NDYsImp0aSI6InRXaEJBd1NUbVhVNnp5UUsxNUV1eXk9PSIsInVzZXIiOnsiaWQiOiIxMjMiLCJuYW1lIjoiTWF5dXJpIn19.cUwgqLmLQJ_JV0vNzdUFNdPcBHk6XTf5GqGSArJSnms"
         self.chat = Talkshoplive.Chat(jwtToken: token, isGuest:false, showKey: showInput)
     }
+    
+    func countMessages() {
+        
+        self.chat?.countMessages({ count, error in
+            if let error = error {
+                print(error.localizedDescription)
+                self.result = "Error fetching messages count: \(error.localizedDescription)"
+            } else {
+                print("Message Count : ", count)
+                self.result = "Message Count : \(count)"
+
+            }
+        })
+    }
+    
 }
 
 struct ChatView_Previews: PreviewProvider {
